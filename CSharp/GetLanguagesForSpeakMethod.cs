45023aae4464054d124716722e3c030dcb19e532
GwendaDecneut <gwendad@cadence3.com>
2016-08-10 09:19:27 -0700

    Create GetLanguagesForSpeakMethod.cs

---
+++ CSharp/GetLanguagesForSpeakMethod.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web;
+namespace MicrosoftTranslatorSdk.SoapSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdmAccessToken admToken;
+            string headerValue;
+            //Get Client Id and Client Secret from https://datamarket.azure.com/developer/applications/
+            AdmAuthentication admAuth = new AdmAuthentication("clientId", "clientSecret");
+            try
+            {
+                admToken = admAuth.GetAccessToken();
+                DateTime tokenReceived = DateTime.Now;
+                // Create a header with the access_token property of the returned token
+                headerValue = "Bearer " + admToken.access_token;
+                GetLanguagesForSpeakMethod(headerValue);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+
+
+        }
+
+        private static void GetLanguagesForSpeakMethod(string authToken)
+        {
+
+            string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.Headers.Add("Authorization", authToken);
+            WebResponse response = null;
+            try
+            {
+                response = httpWebRequest.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+
+                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                    List<string> languagesForSpeak = (List<string>)dcs.ReadObject(stream);
+                    Console.WriteLine("The languages available for speak are: ");
+                    languagesForSpeak.ForEach(a => Console.WriteLine(a));
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+        }
+    }
+}
